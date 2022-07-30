e190402aa0d4bedd9d703bf8942cd27dabe08841
dabhoang <danhoang@ucdavis.edu>
2022-07-29 19:25:17 -0700

    made camera first person POV

--- Assets/Template/Scripts/PlayerMovement.cs
+++ Assets/Template/Scripts/PlayerMovement.cs
@@ -7,13 +7,35 @@ public class PlayerMovement : NetworkBehaviour
 {
 
     Vector3 movementVector;
+    //Vector3 cameraRotation;
+    Quaternion cameraRotation;
     private Rigidbody rb;
+
+    public GameObject camera;
+    Vector3 cameraOffset;
+
 
     // Start is called before the first frame update
     void Start()
     {
         movementVector = new Vector3(0.0f, 0.0f, 0.0f);
+        
+
         rb = GetComponent<Rigidbody>();
+        camera = GameObject.Find("Main Camera");
+
+
+        cameraOffset = new Vector3(0.0f, 10.0f, -10.0f);
+
+        //cameraRotation = new Vector3(45.0f, 0.0f, 0.0f);
+        //cameraRotation = Quaternion.Euler(0.0f, 45.0f, 0.0f);
+        //camera.transform.rotation = cameraRotation;
+        camera.transform.rotation = Quaternion.Euler(45.0f, 0.0f, 0.0f);
+        AdjustCamera();
+
+
+
+        //cameraOffset
     }
 
     // Update is called once per frame
@@ -43,7 +65,12 @@ public class PlayerMovement : NetworkBehaviour
                 movementVector = new Vector3(100.0f, 0.0f, 0.0f);
                 rb.AddForce(movementVector);
             }
-            
+            AdjustCamera();
         }
+    }
+
+    void AdjustCamera()
+    {
+        camera.transform.position = this.transform.position + cameraOffset;
     }
 }
