3719b58569e6f1ce29e1b316a8340f54ea4cd835
dabhoang <danhoang@ucdavis.edu>
2022-07-29 17:47:57 -0700

    deleted unnecessary scripts and removed unnecessary code

--- Assets/Template/Scripts/Player1Movement.cs
+++ Assets/Template/Scripts/Player1Movement.cs
@@ -11,7 +11,6 @@ public class Player1Movement : NetworkBehaviour
     [SyncVar]
     private int playerScore;
 
-    //[SyncVar]
     public TextMeshProUGUI player1ScoreText;
     public TextMeshProUGUI gameOverText;
     GameObject[] players;
@@ -23,56 +22,38 @@ public class Player1Movement : NetworkBehaviour
             rb = GetComponent<Rigidbody>();
             playerScore = 0;
             SetScoreText();
-            //SetCountText();
-            //gameOverText.gameObject.SetActive(false);
         }
-
-        //players = GameObject.FindGameObjectsWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Debug.Log(isLocalPlayer);
         if(isLocalPlayer){
-            //Debug.Log("LOCAL");
             if(Input.GetKeyDown(KeyCode.W))
             {
-                //Debug.Log("Bang W");
                 movementVector = new Vector3(0.0f, 0.0f, 100.0f);
                 rb.AddForce(movementVector);
-                //transform.position = transform.position + movementVector;
             }
 
             if(Input.GetKeyDown(KeyCode.A))
             {
-                //Debug.Log("Bang A");
                 movementVector = new Vector3(-100.0f, 0.0f, 0.0f);
                 rb.AddForce(movementVector);
-                //transform.position = transform.position + movementVector;ss
             }
 
             if(Input.GetKeyDown(KeyCode.S))
             {
-                //Debug.Log("Bang S");
                 movementVector = new Vector3(0.0f, 0.0f, -100.0f);
                 rb.AddForce(movementVector);
-                //transform.position = transform.position + movementVector;
             }
 
             if(Input.GetKeyDown(KeyCode.D))
             {
-                //Debug.Log("Bang D");
                 movementVector = new Vector3(100.0f, 0.0f, 0.0f);
                 rb.AddForce(movementVector);
-                //transform.position = transform.position + movementVector;
             }
             SetScoreText();
         }
-
-
-        
-
         
     }
 
@@ -87,27 +68,19 @@ public class Player1Movement : NetworkBehaviour
         playerScore = newScore;
     }
 
-    //[Command]
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.CompareTag("PickUp"))
         {
             other.gameObject.SetActive(false);
-            //this.gameObject.GetComponent<NetworkIdentity>().AssignClientAuthority(this.GetComponent<NetworkIdentity>().connectionToClient);
             SetPlayerScore(playerScore + 2);
-            //playerScore = playerScore + 2;
-            Debug.Log("SCORE! " + playerScore);
-            //SetCountText();
             SetScoreText(); 
         }
 
         if(other.gameObject.CompareTag("PickUp2"))
         {
             other.gameObject.SetActive(false);
-            //this.gameObject.GetComponent<NetworkIdentity>().AssignClientAuthority(this.GetComponent<NetworkIdentity>().connectionToClient);
             SetPlayerScore(playerScore + 3);
-            //playerScore = playerScore + 3;
-            //SetCountText();
             SetScoreText();
         }
         
@@ -130,29 +103,9 @@ public class Player1Movement : NetworkBehaviour
         }
 
         TextMeshProUGUI textDisplay = GameObject.Find("Player1Score").GetComponent<TextMeshProUGUI>();
-        //player1ScoreText.text = scoreText;
         textDisplay.text = scoreText;
 
-        //player1ScoreText.text = "Player 1's Score: " + player1Score.ToString();
-        /*if(CheckGameOver.GameIsOver())
-        {
-            gameOverText.gameObject.SetActive(true);
-            CheckGameOver.SetGameOverText(gameOverText);
-            
-        }*/
-    }
-
-    
-
-    void SetCountText()
-    {
-        //player1ScoreText.text = "Player 1's Score: " + player1Score.ToString();
-        /*if(CheckGameOver.GameIsOver())
-        {
-            gameOverText.gameObject.SetActive(true);
-            CheckGameOver.SetGameOverText(gameOverText);
-            
-        }*/
+        
     }
 
     
